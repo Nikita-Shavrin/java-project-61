e036dff882d1cb6844aa020cc243af5a5ebce76c
Шаврин Никита <nikita.shavrinn@gmail.com>
2024-02-03 10:34:43 +0300

    finished second lesson

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     application
 }
+
 application {
     mainClass = "hexlet.code.App"
 }
@@ -20,4 +21,12 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
