3317e69cd49cbbd316e3b1986d71fba23cc0488b
Шаврин Никита <nikita.shavrinn@gmail.com>
2024-02-04 13:50:28 +0300

    added a new method choice

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -6,10 +6,24 @@ public class Cli {
     public static void greeting() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("May I have your name?");
+        System.out.println("Welcome to the Brain Games!"
+                +
+                "\nMay I have your name?");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
 
         scanner.close();
+    }
+
+    public static void choice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter."
+                + "\n1 - Greet"
+                + "\n0 - Exit"
+                + "\nYour choice:");
+        int number = Integer.parseInt(scanner.next());
+        if (number == 1) {
+            greeting();
+        }
     }
 }
