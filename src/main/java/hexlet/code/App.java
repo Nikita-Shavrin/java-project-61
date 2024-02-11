ac35b4a09132f39b92ae39cf5e0fb9b7277a1e79
Шаврин Никита <nikita.shavrinn@gmail.com>
2024-02-11 10:25:18 +0300

    added Enginge class and game Calc

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,20 +1,27 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter."
-                + "\n2 - Even"
                 + "\n1 - Greet"
+                + "\n2 - Even"
+                + "\n3 - Calc"
                 + "\n0 - Exit"
                 + "\nYour choice:");
         int number = Integer.parseInt(scanner.next());
+        System.out.println("Welcome to the Brain Games!");
         if (number == 1) {
             Cli.greeting();
         } else if (number == 2) {
             Even.even();
+        } else if (number == 3) {
+            Engine.userInteraction(number);
         }
         scanner.close();
     }
