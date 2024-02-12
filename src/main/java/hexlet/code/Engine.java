e673d950580c4926e3d5e25dc44019c6ea8b1cd4
Шаврин Никита <nikita.shavrinn@gmail.com>
2024-02-12 19:08:11 +0300

    added constructor in Engine and changed Calc

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -6,22 +6,39 @@ import java.util.Scanner;
 
 public class Engine {
     static Scanner scanner = new Scanner(System.in);
-    public static String question;
-    public static String task;
-    public static String correctAnswer;
+
+    public Engine() {
+
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    private static String question;
+    private static String task;
+    private static String correctAnswer;
 
     public static void userInteraction(int gameNumber) {
         System.out.println("May I have your name?");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
         updateTheGame(gameNumber);
-        System.out.println(task);
+        System.out.println();
         int correctAnswers = 0;
         while (correctAnswers < 3) {
             System.out.println("Question: " + question);
             System.out.println("Your answer: ");
             String answer = scanner.next();
-            if (answer.equals(correctAnswer) ) {
+            if (answer.equals(correctAnswer)) {
                 System.out.println("Correct!");
                 correctAnswers++;
             } else {
@@ -33,8 +50,8 @@ public class Engine {
         }
         System.out.println("Congratulations, " + userName + "!");
         scanner.close();
-
     }
+
     public static void updateTheGame(int gameNumber) {
         if (gameNumber == 3) {
             Calc.calc();
--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -6,15 +6,16 @@ import java.util.Scanner;
 
 public class Calc {
     public static void calc() {
-        Engine.task = "What is the result of the expression?";
+        Engine calc = new Engine();
+        calc.setTask("What is the result of the expression?");
         int number1 = (int) (Math.random() * 100) + 1;
         int number2 = (int) (Math.random() * 100) + 1;
         int rundomAction = (int) (Math.random() * 3) + 1;
         char action = (rundomAction == 1) ? '+' : (rundomAction == 2) ? '-'
                 : (rundomAction == 3) ? '*' : null;
-         Engine.correctAnswer =  String.valueOf((rundomAction == 1) ? number1 + number2 :
-                (rundomAction == 2) ? number1 - number2 :
-                        (rundomAction == 3) ? number1 * number2 : null);
-        Engine.question = String.valueOf((number1 + " " + action + " " + number2));
+         calc.setCorrectAnswer(String.valueOf((rundomAction == 1) ? number1 + number2 :
+                 (rundomAction == 2) ? number1 - number2 :
+                         (rundomAction == 3) ? number1 * number2 : null));
+        calc.setQuestion(String.valueOf((number1 + " " + action + " " + number2)));
     }
 }
