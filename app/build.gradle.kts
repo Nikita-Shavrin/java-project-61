aa816f36b731b4a30ee3c4512e0b0ed6c902851f
Nikita Shavrin <138622218+Nikita-Shavrin@users.noreply.github.com>
2024-02-16 12:26:18 +0300

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
